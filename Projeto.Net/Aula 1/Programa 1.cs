06e66ef8b7379a7372c34d6917e77ac6b0cecfb1
Mateus Fonseca <mateus.fonseca1992@gmail.com>
2023-05-17 11:32:16 -0300

    2
    2

---
+++ Projeto.Net/Aula 1/Programa 1.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Programa{
+       
+    static void Main()
+{
+
+    /*1*/
+    Console.WriteLine("Digite a largura do lote:");
+    double largura= Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Digite o comprimento do lote:");
+    double comprimento= Convert.ToDouble(Console.ReadLine());
+
+    double area= largura * comprimento;
+
+    Console.WriteLine("A area do terrreno é:"+ area+" mestros quadrados");
+
+    /*2*/
+
+    Console.WriteLine("Digite a quatidade de cavalos:");
+    int cavalos=Convert.ToInt16(Console.ReadLine());
+
+    int ferraduras= cavalos *4;
+    Console.WriteLine("Serão necessários "+ferraduras+"ferraduras,para ferrar "+cavalos+" cavalos");
+
+    /*3*/
+    Console.WriteLine("Digite seu nome");
+    string nome= Console.ReadLine();
+
+    Console.WriteLine("Digite a sua idade");
+    int idade= Convert.ToInt16(Console.ReadLine());
+
+    int dias= idade*365;
+
+    Console.WriteLine(nome.ToUpper()+" você já viveu "+dias+" dias de vida.");
+
+}
+}
+
---
+++ Projeto.Net/Aula 2/EstruturasRepetiçaoControle.cs
@@ -0,0 +1,71 @@
+﻿public class EstruturasRepetiçaoControle{
+    static void Main(string[] args){
+  
+ int[] resultado_mega = {10, 29, 40, 53, 69, 78};
+
+      int[] palpite_mega = new int[6];
+
+      int n=0, numRepetido = 0;
+
+      string entrada_usuario;
+
+        for(int p = 0; p<palpite_mega.Count(); p++)
+        {
+            Console.WriteLine("Digite o valor do seu " + (p+1) + " palpite: ");
+            entrada_usuario = Console.ReadLine();
+            n = int.Parse(entrada_usuario);
+
+            if ((n > 0) && (n < 100))//intervalo da mega sena
+            {
+                palpite_mega[p] = n;
+                n = 0;
+                entrada_usuario = "";            
+            }
+            else
+            {
+                Console.WriteLine("Numero inválido. Repita a operação utilizando numeros maiores que 0 e menores do que 100.");
+                break;
+            } 
+            
+        }
+        for(int i = 0; i<palpite_mega.Count(); i++)
+        {
+            for(int j = i+1; j < palpite_mega.Count(); j++)
+            {
+                if(palpite_mega[i] == palpite_mega[j])
+                {
+                    numRepetido = 1;
+                }
+            }
+        }
+        if(numRepetido == 1)
+        {
+            Console.WriteLine("NUMEROS IGUAIS DIGITADOS. FAVOR REPETIR A OPERAÇÃO.");
+        }else if (palpite_mega[5] == 0)
+        {
+            Console.WriteLine("REPITA A OPERAÇÃO.");
+        }else
+        {
+            Console.WriteLine("O palpite digitado foi");
+            for (int p = 0; p < palpite_mega.Count(); p++)
+            {
+
+                Console.Write("\t" + palpite_mega[p]);
+            }
+        }
+    }
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}
